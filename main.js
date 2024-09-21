db63d76dd85f4a1ab365df40fcb94e25f845f65e
Veronica Capobianco <veritocapito@gmail.com>
2024-09-21 20:23:24 -0300

    add hero

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ let contentHtml = ''
 
 let counter = 1
 let offset = 0
+let total = 80
 
-
+//Fetching
 function getComics() {
     offset = (parseInt(20) * parseInt(counter)) - parseInt(20);
     console.log(offset);
@@ -31,37 +32,45 @@ function setComics(comics) {
 
         contentHtml += `
         <div class="card col-md-4 gap-2 justify-items-between" style="width: 18rem;">
-            <img src="${comicImg}" class="card-img-top img-fluid" alt="${comicTitle}">
-            <div class="card-body">
-                <h5 class="card-title">${comicTitle}</h5>
+            <img src="${comicImg}" class="card-img-top h-100" alt="${comicTitle}">
+            <div class="card-body gap-2 mt-2 h-20">
+                <h5 class="card-title mt-2">${comicTitle}</h5>
                 <a href="${comicUrl}" target="_blank" class="btn btn-danger">More info</a>
             </div>
         </div>`;
 
-        /*         console.log(comicTitle);
-                console.log(comicImg);
-                console.log(comicUrl);
-                console.log(contentHtml); */
     }
     container.innerHTML = contentHtml;
 }
 
-//Paginado
+//Paging
 const add = document.querySelector('#add');
 const subs = document.querySelector('#subs');
+const firstPage = document.querySelector('#first-page');
+const lastPage = document.querySelector('#last-page');
 const pages = document.querySelector('#pages');
 
 
 add.addEventListener('click', () => {
-    pages.textContent = ++counter
+    pages.textContent = counter < 6 ? ++counter : pages.textContent
     getComics()
 });
 subs.addEventListener('click', () => {
     pages.textContent = counter > 1 ? --counter : pages.textContent
     getComics()
 });
+firstPage.addEventListener('click', () => {
+    counter = 1
+    pages.textContent = counter
+    getComics()
+});
+lastPage.addEventListener('click', () => {
+    counter = 6
+    pages.textContent = counter
+    getComics()
+});
 
 
-//Parametros de busqueda
-const params = apiUrl.searchParams
+//Search Params
+//const params = apiUrl.searchParams
 
