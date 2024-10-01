ca67e85416457ad24f28a66d764045ae6ad88d53
Veronica Capobianco <veritocapito@gmail.com>
2024-10-01 20:11:27 -0300

    add characters search function

--- main.js
+++ main.js
@@ -2,25 +2,33 @@ const container = document.getElementById('container')
 let contentHtml = ''
 
 let counter = 1
-let offset = 20
-let total = 160
-let limit = 16
+let query = 'characters'
+
+const ak = 'd0bac062c296448e513e8a3147810f19'
+const hs = '5e6eef74abfa220a102df0024190f6e7'
+
+
+const url = new URL(`https://gateway.marvel.com:443/v1/public/${query}`)
+url.searchParams.set('ts', '1')
+url.searchParams.set('apikey', ak)
+url.searchParams.set('hash', hs)
 
 //Fetching
-function getComics() {
-    offset = (parseInt(limit) * parseInt(counter)) - parseInt(limit);
-    console.log(offset);
-    fetch(`https://gateway.marvel.com:443/v1/public/comics?limit=${limit}&offset=${offset}&ts=1&apikey=d0bac062c296448e513e8a3147810f19&hash=5e6eef74abfa220a102df0024190f6e7`)
+function getData() {
+    console.log(url);
+    
+    fetch(url)
         .then(res => res.json())
-        .then(info => setComics(info.data.results))
+        .then(info => query === 'comics'? setComics(info.data.results) : setCharacters(info.data.results))            
         .catch(err => console.log(err))
 }
 
-getComics();
+getData();
 
 function setComics(comics) {
     contentHtml = '';
     console.log(comics);
+        //offset = (parseInt(limit) * parseInt(counter)) - parseInt(limit);
 
     for (let i = 0; i < comics.length; i++) {
         const comicTitle = comics[i].title;
@@ -56,6 +64,32 @@ function setComics(comics) {
     container.innerHTML = contentHtml;
 }
 
+function setCharacters(characters) {
+    contentHtml = '';
+    console.log(characters);
+
+    for (let i = 0; i < characters.length; i++) {
+        const characterImg = `${characters[i].thumbnail.path}.${characters[i].thumbnail.extension}`;
+        const characterName = characters[i].name;
+        const characterComics = characters[i].comics ;     
+
+        contentHtml += `
+        <div class="card col-md-4 mt-3 mb-3 comic-card" id="character">
+            <div class="img-container">
+                <img src="${characterImg}" class="card-img img-fluid" alt="${characterName}">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">${characterName}</h5>
+                <p class="card-text">Comics: ${characterComics.available}</p>
+            </div>
+        </div>`;
+
+    }
+    container.innerHTML = contentHtml;
+}
+
+
+
 //Paging
 const add = document.querySelector('#add');
 const subs = document.querySelector('#subs');
@@ -66,21 +100,21 @@ const pages = document.querySelector('#pages');
 
 add.addEventListener('click', () => {
     pages.textContent = counter < 10 ? ++counter : pages.textContent
-    getComics()
+    getData()
 });
 subs.addEventListener('click', () => {
     pages.textContent = counter > 1 ? --counter : pages.textContent
-    getComics()
+    getData()
 });
 firstPage.addEventListener('click', () => {
     counter = 1
     pages.textContent = counter
-    getComics()
+    getData()
 });
 lastPage.addEventListener('click', () => {
     counter = 10
     pages.textContent = counter
-    getComics()
+    getData()
 });
 
 
