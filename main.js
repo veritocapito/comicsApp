c06bbf3121119a3010b4b7cce41a6ae80794d088
Veronica Capobianco <veritocapito@gmail.com>
2024-10-02 23:42:01 -0300

    add setCharacterComics function

--- main.js
+++ main.js
@@ -15,11 +15,10 @@ url.searchParams.set('hash', hs)
 
 //Fetching
 function getData() {
-    console.log(url);
-    
-    fetch(url)
+
+    fetch(`${url}`)
         .then(res => res.json())
-        .then(info => query === 'comics'? setComics(info.data.results) : setCharacters(info.data.results))            
+        .then(info => query === 'comics' ? setComics(info.data.results) : setCharacters(info.data.results))
         .catch(err => console.log(err))
 }
 
@@ -28,7 +27,7 @@ getData();
 function setComics(comics) {
     contentHtml = '';
     console.log(comics);
-        //offset = (parseInt(limit) * parseInt(counter)) - parseInt(limit);
+    //offset = (parseInt(limit) * parseInt(counter)) - parseInt(limit);
 
     for (let i = 0; i < comics.length; i++) {
         const comicTitle = comics[i].title;
@@ -66,26 +65,70 @@ function setComics(comics) {
 
 function setCharacters(characters) {
     contentHtml = '';
-    console.log(characters);
-
     for (let i = 0; i < characters.length; i++) {
+        const characterId = characters[i].id;
         const characterImg = `${characters[i].thumbnail.path}.${characters[i].thumbnail.extension}`;
         const characterName = characters[i].name;
-        const characterComics = characters[i].comics ;     
+        const characterComics = characters[i].comics.available;     
 
         contentHtml += `
-        <div class="card col-md-4 mt-3 mb-3 comic-card" id="character">
+        <div class="card col-md-4 mt-3 mb-3 comic-card" data-id="${characterId}" data-name="${characterName}" data-img="${characterImg}">
             <div class="img-container">
                 <img src="${characterImg}" class="card-img img-fluid" alt="${characterName}">
             </div>
             <div class="card-body">
                 <h5 class="card-title">${characterName}</h5>
-                <p class="card-text">Comics: ${characterComics.available}</p>
+                <p class="card-text">Comics: ${characterComics}</p>
             </div>
         </div>`;
-
     }
     container.innerHTML = contentHtml;
+
+    document.querySelectorAll('.comic-card').forEach(card => {
+        card.addEventListener('click', function() {
+            const characterId = this.getAttribute('data-id');
+            const characterName = this.getAttribute('data-name');
+            const characterImg = this.getAttribute('data-img');
+            setCharacterComics(characterId, characterName, characterImg);
+        });
+    });
+}
+
+
+function setCharacterComics(characterId, characterName, characterImg) {
+    contentHtml = ``;
+
+    fetch(`https://gateway.marvel.com:443/v1/public/characters/${characterId}/comics?ts=1&apikey=${ak}&hash=${hs}`)
+        .then(res => res.json())
+        .then(info => {
+            const comics = info.data.results;
+
+            contentHtml += `
+            <div class="col-12 mt-3 mb-3">
+                <div class="card-header col-md-4 mt-3 mb-3">
+                    <img src="${characterImg}" class="card-img img-thumbnail col-4" alt="${characterName}">
+                    <h3 class="mt-3 mb-3">${characterName}</h3>
+                </div>
+            </div>`
+
+            for (let i = 0; i < comics.length; i++) {
+                const comicTitle = comics[i].title;
+                const comicImg = `${comics[i].thumbnail.path}.${comics[i].thumbnail.extension}`;
+                const comicUrl = comics[i].urls[0].url; // URL del comic
+
+                contentHtml += `
+                <div class="card col-md-4 mt-3 mb-3 comic-card">
+                    <div class="img-container">
+                        <img src="${comicImg}" class="card-img img-fluid" alt="${comicTitle}">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title"><a href="${comicUrl}" target="_blank">${comicTitle}</a></h5>
+                    </div>
+                </div>`;
+            }
+            container.innerHTML = contentHtml;
+        })
+        .catch(err => console.log(err));
 }
 
 
@@ -118,6 +161,4 @@ lastPage.addEventListener('click', () => {
 });
 
 
-//Search Params
-//const params = apiUrl.searchParams
 
